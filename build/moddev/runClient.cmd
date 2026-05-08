c323185bed82a5391a95a2092fec14fc7c0e0e6c
ItzGreenCat <greencat666@outlook.com>
2026-05-08 09:11:26 +0800

    chore: remove untracked build, idea and gradle files

--- build/moddev/runClient.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-for /f "tokens=2 delims=:." %%x in ('chcp') do set _codepage=%%x
-chcp 65001>nul
-cd C:\Users\greencat.GREENCAT-PC\IdeaProjects\CreatePinkSpiral\run
-"C:\Program Files\Java\jdk-21\bin\java.exe" @C:\Users\greencat.GREENCAT-PC\IdeaProjects\CreatePinkSpiral\build\moddev\clientRunClasspath.txt @C:\Users\greencat.GREENCAT-PC\IdeaProjects\CreatePinkSpiral\build\moddev\clientRunVmArgs.txt -Dfml.modFolders=createpinkspiral%%%%C:\Users\greencat.GREENCAT-PC\IdeaProjects\CreatePinkSpiral\build\classes\java\main;createpinkspiral%%%%C:\Users\greencat.GREENCAT-PC\IdeaProjects\CreatePinkSpiral\build\resources\main net.neoforged.devlaunch.Main @C:\Users\greencat.GREENCAT-PC\IdeaProjects\CreatePinkSpiral\build\moddev\clientRunProgramArgs.txt
-if not ERRORLEVEL 0 (  echo Minecraft failed with exit code %ERRORLEVEL%  pause)
-chcp %_codepage%>nul
-endlocal
